887c51d51eaa6b919764b396fa23186b23e086d4
Andrew Bullock <trullock@gmail.com>
2017-06-06 19:54:08 +0100

    separated r2d2js into logical components

--- r2d2.js
+++ r2d2.js
@@ -1,211 +1,24 @@
-var pi_gpio = require('pi-gpio');
 var express = require('express');
 var Sound = require('node-aplay');
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
-var GPIO = function(){
-	return {
-		init: function(pin) {
-			console.info('Closing GPIO' + pin);
-			return new Promise(function(resolve){
-				pi_gpio.close(pin, function(){
-					resolve()
-				});
-			});
-		},
-		
-		mode: function(pin, mode){
-			console.info('Configuring GPIO pin ' + pin + ' to ' + mode);
-			return new Promise(function(resolve){
-				pi_gpio.open(pin, mode, function(){
-					resolve()
-				});
-			});
-		},
-		
-		set: function(pin) {
-			console.log('Setting GPIO pin ' + pin + ' high');
-			
-			return new Promise(function(resolve){
-				pi_gpio.write(pin, 1, function(){
-					resolve()
-				});
-			});
-		},
-		
-		unset: function(pin) {
-			console.log('Setting GPIO pin ' + pin + ' low');
-			return new Promise(function(resolve){
-				pi_gpio.write(pin, 0, function(){
-					resolve()
-				});
-			});
-		}
-	};
-}
+var GPIO = require('./lib/gpio.js');
+var Logic = require('./lib/logic.js');
+var Voice = require('./lib/voice.js');
 
 var Droid = function(gpio){
 
-	var Logic = function(pins){
-	
-		var timerInterval,
-			alternateColorIndex;
-		
-		var colors = [];
-		
-		for(var color in pins) {
-			if(!pins.hasOwnProperty(color)) continue;
-			colors.push(color);
-		}
-				
-		function allColors(delegate){
-			var promises = [];
-			
-			for(var i = 0; i < colors.length; i++)
-				promises.push(delegate(i, colors[i], pins[colors[i]]));
-			
-			return Promise.all(promises);
-		} 
-		
-		function init(){
-			return allColors(function(i, color, pin){
-				return gpio.init(pins[colors[i]]).then(function() {
-					return gpio.mode(pins[colors[i]], 'output');
-				});
-			});
-		}
-		
-		function reset(){
-			clearInterval(timerInterval);
-			
-			return allColors(function(i, color, pin) {
-				return off(color);
-			});
-		}
-		
-		function on(color){
-			return gpio.set(pins[color]);
-		}
-		
-		function off(color){
-			return gpio.unset(pins[color]);
-		}
-		
-		return {
-			
-			init: function(){
-				return init().then(function(){
-					return reset();
-				});
-			},
-			
-			off: function(){
-				return reset();
-			},
-			
-			solid: function(color) {
-				if(!pins.hasOwnProperty(color))
-					throw Error('Color ' + color + ' not recognised');
-				
-				reset().then(function(){
-					on(color);
-				});
-			},
-			
-			all: function(){
-				reset();
-				
-				allColors(function(i, color, pin){
-					on(color);
-				});
-			},
-			
-			cycle: function(interval) {
-				reset();
-			
-				alternateColorIndex = 0;
-				timerInterval = setInterval(function() {
-					off(colors[alternateColorIndex]);
-					
-					alternateColorIndex++;
-					if(alternateColorIndex == colors.length)
-						alternateColorIndex = 0;
-					
-					on(colors[alternateColorIndex]);
-				}, interval);
-			},
-			
-			random: function(interval) {
-				reset();
-				
-				timerInterval = setInterval(function(){
-					allColors(function(i, color, pin){
-						var r = Math.random();
-						if(r >= 0.5)
-							on(color);
-						else
-							off(color);
-					});
-				}, interval);
-			}
-		};
-	};
-	
-	var Voice = function(){
-		
-		var happy = new Sound('R2D2a.wav');
-		var complain = new Sound('R2D2c.wav');
-		var chirp = new Sound('R2D2c.wav');
-		
-		function setVolume(percent){
-			console.info('Setting volume to ' + percent + '%');
-			
-			return new Promise(function(resolve, reject){
-				var child = exec('amixer set PCM ' + percent + '%', function(error, stdout, stderror) {
-					if(error)
-					{
-						console.error('Error setting volume');
-						console.error(error);
-						reject(error);
-					}
-					else
-						resolve();
-				})
-			});
-		}		
-		
-		return {
-			init: function(){
-				return setVolume(100);
-			},
-			
-			happy: function(){
-				happy.play();
-			},
-			
-			complain: function(){
-				complain.play();
-			},
-			
-			chirp: function(){
-				chirp.play();
-			},
-			
-			volume: function(percent) {
-				percent = Math.round(percent);
-				
-				return setVolume(percent);
-			}
-		}
-	}
-	
 	var me = {
 		frontPSI: new Logic({ 'red': 38, 'blue': 40 }),
 		rearPSI: new Logic({ 'yellow': 32, 'green': 36 }),
 		logic: new Logic({ 'a': 13, 'b': 15, 'c': 29, 'd': 31, 'e': 33, 'f': 35, 'g': 37 }),
 		frontHolo: new Logic({ 'white': 7 }),
-		speak: new Voice()
+		voice: new Voice({
+			'happy': 'R2D2a.wav',
+			'angry': 'R2D2c.wav',
+			'chirp': 'R2D2d.wav'
+		})
 	}
 	
 	me.behaviour = {
@@ -219,7 +32,7 @@ var Droid = function(gpio){
 			me.frontPSI.cycle(100);
 			me.rearPSI.cycle(100);
 			me.logic.all();
-			me.speak.chirp();
+			me.voice.say('chirp');
 			
 			setTimeout(function(){
 				me.behaviour.idle();
@@ -229,7 +42,7 @@ var Droid = function(gpio){
 		celebrate: function(){
 			me.frontPSI.cycle(100);
 			me.rearPSI.cycle(100);
-			me.speak.happy();
+			me.voice.say('happy');
 			
 			setTimeout(function(){
 				me.behaviour.idle();
@@ -239,7 +52,7 @@ var Droid = function(gpio){
 		complain: function(){
 			me.frontPSI.solid('red');
 			me.rearPSI.solid('yellow');
-			me.speak.angry();
+			me.voice.say('angry');
 			
 			setTimeout(function(){
 				me.behaviour.idle();
@@ -361,19 +174,19 @@ app.post('/speak', function(req, res){
 	
 	switch(message){
 		case "happy":
-			r2d2.speak.happy();
-			break;
-		case "alert":
-			r2d2.speak.alert();
+		case "angry":
+		case "chirp":
+			r2d2.voice.say(message);
 			break;
 	}
+	
 	res.sendStatus(200);
 });
 
 app.post('/speak/volume', function(req, res){
 	var vol = parseInt(req.body.volume, 10);
 	
-	r2d2.speak.volume(vol);
+	r2d2.voice.volume(vol);
 	
 	res.sendStatus(200);
 });
