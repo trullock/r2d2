99eea9d0d9fbb2a776758bd21aec4091e432a4db
Andrew Bullock <trullock@gmail.com>
2017-12-02 16:33:42 +0000

    experimenting with leds

--- leds.js
+++ leds.js
@@ -1,7 +1,7 @@
 var ws281x = require('rpi-ws281x-native');
 
 var numLeds = 48;
-var data = new UInt32Array(numLeds);
+var data = new Uint32Array(numLeds);
 
 function setFrontLogic(){
 	for(var i = 0; i < 32; i++) {
@@ -15,6 +15,6 @@ ws281x.init(numLeds);
 
 setInterval(function(){
 	setFrontLogic();
-	ws218x.rander(data);
+	ws281x.rander(data);
 	
 }, 250);
