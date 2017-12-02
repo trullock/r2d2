c576507b4655657f9c369b246d10d0d18584f06f
Andrew Bullock <trullock@gmail.com>
2017-12-02 16:38:20 +0000

    experimenting with leds

--- leds.js
+++ leds.js
@@ -11,11 +11,27 @@ function setFrontLogic(){
 	}
 }
 
+function setRearLogic(){
+	for(var i = 32; i < 48; i++) {
+		var color = Math.random();
+		if(color > 0.66
+			color = 0xff0000;
+		else if(color > 0.33)
+			color = 0x00ff00;
+		else
+			color = 0xffff00;
+		
+		var on = Math.random() > 0.5;
+		data[i] = on ? color : 0x000000;
+	}
+}
+
 ws281x.init(numLeds);
-ws281x.setBrightness(64);
+ws281x.setBrightness(32);
 
 setInterval(function(){
 	setFrontLogic();
+	setRearLogic();
 	ws281x.render(data);
 	
 }, 150);
